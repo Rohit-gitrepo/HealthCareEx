ff3a24d9d5880f26b4eb265c420206b30430003f
Rohit Kumar <rkc2772@gmail.com>
2021-11-17 14:26:47 +0530

    spec service and impl

---
+++ src/main/java/in/nareshit/raghu/service/impl/ISpecializationServiceImpl.java
@@ -0,0 +1,50 @@
+package in.nareshit.raghu.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import in.nareshit.raghu.entity.Specialization;
+import in.nareshit.raghu.repo.SpecializationRepository;
+import in.nareshit.raghu.service.ISpecializationService;
+
+public class ISpecializationServiceImpl implements ISpecializationService {
+	
+	@Autowired
+	private SpecializationRepository repo;
+
+	@Override
+	public Long saveSpecialization(Specialization spec) {
+		return repo.save(spec).getId();
+	}
+
+	@Override
+	public List<Specialization> getAllSpecialization() {		
+		return repo.findAll();
+	}
+
+	@Override
+	public void updateSpecialization(Specialization spec) {
+	repo.save(spec);
+
+	}
+	
+	@Override
+	public void removeSpecialization(Long id) {
+		repo.deleteById(id);
+		
+	}
+
+	@Override
+	public Specialization getOneSpecialization(Long id) {
+		Optional<Specialization> optional = repo.findById(id);
+		if(optional.isPresent()) {
+			return optional.get();
+		}else {
+			return null;
+		}
+
+	}
+
+}
